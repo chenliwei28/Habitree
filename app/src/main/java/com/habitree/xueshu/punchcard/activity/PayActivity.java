22426e9cfb50a2ceb3c585b7bd9c0479527fce57
chen <chenlifei1@163.com>
2018-02-05 17:49:08 +0800

    2/5

--- app/src/main/java/com/habitree/xueshu/punchcard/activity/PayActivity.java
+++ app/src/main/java/com/habitree/xueshu/punchcard/activity/PayActivity.java
@@ -1,13 +1,23 @@
 package com.habitree.xueshu.punchcard.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.habitree.xueshu.R;
 import com.habitree.xueshu.xs.activity.BaseActivity;
 
-public class PayActivity extends BaseActivity {
+public class PayActivity extends BaseActivity implements View.OnClickListener{
 
+    private LinearLayout mWxCheckLl;
+    private LinearLayout mAliCheckLl;
+    private LinearLayout mBalanceCheckLl;
+    private ImageView mWxCheckIv;
+    private ImageView mAliCheckIv;
+    private ImageView mBalanceCheckIv;
+
+    private int mCurrentMode = -1;
 
     @Override
     protected int setLayoutId() {
@@ -16,16 +26,60 @@ public class PayActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        mWxCheckLl = findViewById(R.id.wx_check_ll);
+        mAliCheckLl = findViewById(R.id.ali_check_ll);
+        mBalanceCheckLl = findViewById(R.id.balance_check_ll);
+        mWxCheckIv = findViewById(R.id.wx_check_iv);
+        mAliCheckIv = findViewById(R.id.ali_check_iv);
+        mBalanceCheckIv = findViewById(R.id.balance_check_iv);
     }
 
     @Override
     protected void initListener() {
-
+        mWxCheckLl.setOnClickListener(this);
+        mAliCheckLl.setOnClickListener(this);
+        mBalanceCheckLl.setOnClickListener(this);
     }
 
     @Override
     protected void initData() {
+        selectMode(0);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.wx_check_ll:
+                selectMode(0);
+                break;
+            case R.id.ali_check_ll:
+                selectMode(1);
+                break;
+            case R.id.balance_check_ll:
+                selectMode(2);
+                break;
+        }
+    }
+
+    private void selectMode(int position){
+        if (mCurrentMode==position)return;
+        mCurrentMode = position;
+        switch (position){
+            case 0:
+                mWxCheckIv.setSelected(true);
+                mAliCheckIv.setSelected(false);
+                mBalanceCheckIv.setSelected(false);
+                break;
+            case 1:
+                mWxCheckIv.setSelected(false);
+                mAliCheckIv.setSelected(true);
+                mBalanceCheckIv.setSelected(false);
+                break;
+            case 2:
+                mWxCheckIv.setSelected(false);
+                mAliCheckIv.setSelected(false);
+                mBalanceCheckIv.setSelected(true);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/habitree/xueshu/xs/view/AppleDialog.java
@@ -0,0 +1,211 @@
+package com.habitree.xueshu.xs.view;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.habitree.xueshu.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleDialog {
+    private Context context;
+    private Dialog dialog;
+    private TextView txt_title;
+    private TextView txt_cancel;
+    private LinearLayout lLayout_content;
+    private ScrollView sLayout_content;
+    private boolean showTitle = false;
+    private List<SheetItem> sheetItemList;
+    private Display display;
+    private Resources mResource;
+
+    public AppleDialog(Context context){
+        this.context = context;
+        WindowManager windowManager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        display = windowManager.getDefaultDisplay();
+        mResource=context.getResources();
+    }
+
+    public AppleDialog builder() {
+        // 获取Dialog布局
+        View view = LayoutInflater.from(context).inflate(
+                R.layout.dialog_apple, null);
+
+        // 设置Dialog最小宽度为屏幕宽度
+        view.setMinimumWidth(display.getWidth());
+
+        // 获取自定义Dialog布局中的控件
+        sLayout_content = view.findViewById(R.id.sLayout_content);
+        lLayout_content = view.findViewById(R.id.lLayout_content);
+        txt_title = view.findViewById(R.id.txt_title);
+        txt_cancel = view.findViewById(R.id.txt_cancel);
+        txt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        // 定义Dialog布局和参数
+        dialog = new Dialog(context, R.style.AppleDialog);
+        dialog.setContentView(view);
+        Window dialogWindow = dialog.getWindow();
+        dialogWindow.setGravity(Gravity.START | Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.x = 0;
+        lp.y = 0;
+        dialogWindow.setAttributes(lp);
+
+        return this;
+    }
+
+    public AppleDialog setTitle(String title) {
+        showTitle = true;
+        txt_title.setVisibility(View.VISIBLE);
+        txt_title.setText(title);
+        return this;
+    }
+
+    public AppleDialog setCancelable(boolean cancel) {
+        dialog.setCancelable(cancel);
+        return this;
+    }
+
+    public AppleDialog setCanceledOnTouchOutside(boolean cancel) {
+        dialog.setCanceledOnTouchOutside(cancel);
+        return this;
+    }
+
+    /**
+     *
+     * @param strItem
+     *            条目名称
+     * @param color
+     *            条目字体颜色，设置null则默认蓝色
+     * @param listener
+     * @return
+     */
+    public AppleDialog addSheetItem(String strItem, int color,
+                                          OnSheetItemClickListener listener) {
+        if (sheetItemList == null) {
+            sheetItemList = new ArrayList<SheetItem>();
+        }
+        sheetItemList.add(new SheetItem(strItem, color, listener));
+        return this;
+    }
+
+    /** 设置条目布局 */
+    private void setSheetItems() {
+        if (sheetItemList == null || sheetItemList.size() <= 0) {
+            return;
+        }
+
+        int size = sheetItemList.size();
+
+        // 添加条目过多的时候控制高度
+        if (size >= 7) {
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) sLayout_content
+                    .getLayoutParams();
+            params.height = display.getHeight() / 2;
+            sLayout_content.setLayoutParams(params);
+        }
+
+        // 循环添加条目
+        for (int i = 1; i <= size; i++) {
+            final int index = i;
+            SheetItem sheetItem = sheetItemList.get(i - 1);
+            String strItem = sheetItem.name;
+            int color = sheetItem.color;
+            final OnSheetItemClickListener listener = sheetItem.itemClickListener;
+
+            TextView textView = new TextView(context);
+            textView.setText(strItem);
+            textView.setTextSize(18);
+            textView.setGravity(Gravity.CENTER);
+
+            // 背景图片
+            if (size == 1) {
+                if (showTitle) {
+                    textView.setBackgroundResource(R.drawable.shape_apple_dialog_bottom);
+                } else {
+                    textView.setBackgroundResource(R.drawable.shape_apple_dialog_single);
+                }
+            } else {
+                if (showTitle) {
+                    if (i >= 1 && i < size) {
+                        textView.setBackgroundResource(R.drawable.shape_apple_dialog_mid);
+                    } else {
+                        textView.setBackgroundResource(R.drawable.shape_apple_dialog_bottom);
+                    }
+                } else {
+                    if (i == 1) {
+                        textView.setBackgroundResource(R.drawable.shape_apple_dialog_top);
+                    } else if (i < size) {
+                        textView.setBackgroundResource(R.drawable.shape_apple_dialog_mid);
+                    } else {
+                        textView.setBackgroundResource(R.drawable.shape_apple_dialog_bottom);
+                    }
+                }
+            }
+
+            // 字体颜色
+            if (color == 0) {
+                textView.setTextColor(mResource.getColor(R.color.blue));
+            } else {
+                textView.setTextColor(mResource.getColor(color));
+            }
+
+            // 高度
+            float scale = context.getResources().getDisplayMetrics().density;
+            int height = (int) (45 * scale + 0.5f);
+            textView.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, height));
+
+            // 点击事件
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onClick(index);
+                    dialog.dismiss();
+                }
+            });
+
+            lLayout_content.addView(textView);
+        }
+    }
+
+    public void show() {
+        setSheetItems();
+        dialog.show();
+    }
+
+    public interface OnSheetItemClickListener {
+        void onClick(int which);
+    }
+
+    public class SheetItem {
+        String name;
+        OnSheetItemClickListener itemClickListener;
+        int color;
+
+        public SheetItem(String name, int color,
+                         OnSheetItemClickListener itemClickListener) {
+            this.name = name;
+            this.color = color;
+            this.itemClickListener = itemClickListener;
+        }
+    }
+}
