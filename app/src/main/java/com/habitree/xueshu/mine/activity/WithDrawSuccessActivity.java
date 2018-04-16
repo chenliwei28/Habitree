4eda7b8bf351bd12f543a72586adbc0744593238
ALollies <a2java@163.com>
2018-04-16 14:12:58 +0800

    修改图片选择闪退问题（8.0系统以上出现）

---
+++ app/src/main/java/com/habitree/xueshu/mine/activity/WithDrawSuccessActivity.java
@@ -0,0 +1,32 @@
+package com.habitree.xueshu.mine.activity;
+
+import com.habitree.xueshu.R;
+import com.habitree.xueshu.xs.activity.BaseActionBarActivity;
+
+/**
+ * 提现成功
+ *
+ * @author wuxq
+ */
+public class WithDrawSuccessActivity extends BaseActionBarActivity {
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_withdraw_success;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
